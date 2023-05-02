50a876bdc76f46acf809d8c86f516a27417f2a96
rh-abir <rhabir.co@gmail.com>
2023-05-02 11:08:28 +0600

    tailwind and dasiui set up

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Main = () => {
-    return (
-        <div>
-            <h2>This is layout page</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default Main;

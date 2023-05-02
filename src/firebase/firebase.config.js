735ad332b3d0812dc88ff70dd0c4f6c6c08bf05c
rh-abir <rhabir.co@gmail.com>
2023-05-02 21:44:17 +0600

    create singup page and firebase setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC03kErHLlMQe9KsCNkr5g4H4I0naX7Qz4",
+  authDomain: "food-cafe-fc1fd.firebaseapp.com",
+  projectId: "food-cafe-fc1fd",
+  storageBucket: "food-cafe-fc1fd.appspot.com",
+  messagingSenderId: "612864403866",
+  appId: "1:612864403866:web:ed698c4d824dd8a560b223",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Register = () => {
-    return (
-        <div>
-            <h2>This is Register page</h2>
+  const handleSingup = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const imgUrl = form.imgurl.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, imgUrl, email, password);
+  };
+  return (
+    <form onSubmit={handleSingup}>
+      <div className="hero min-h-full bg-base-200">
+        <div className="hero-content flex-col ">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text ">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Your Name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text ">Image Url</span>
+                </label>
+                <input
+                  type="text"
+                  name="imgurl"
+                  placeholder="image url"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text ">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text ">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn btn-primary"
+                  type="submit"
+                  value="Sing Up"
+                />
+              </div>
+              <label className="label">
+                <Link
+                  to="/login"
+                  className="label-text-alt link link-hover text-lg"
+                >
+                Already Have  An Account ?
+                </Link>
+              </label>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </form>
+  );
 };
 
 export default Register;

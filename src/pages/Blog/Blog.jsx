e7590fe1721cb51422de8dab89390d12b0cd7966
rh-abir <rhabir.co@gmail.com>
2023-05-05 09:12:14 +0600

    firebase deploay and pdf file download ada

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import Pdf from "react-to-pdf";
 
+const ref = React.createRef();
 
 const Blog = () => {
   return (
-    <div className="md:px-20 flex mt-20">
-      <div className=" w-[70%] mx-auto my-40">
+    <div className="md:px-20 flex mt-16">
+      <div ref={ref} className=" w-[70%] mx-auto my-40">
         <div>
           <h2 className="font-bold">
             1. Tell us the differences between uncontrolled and controlled
@@ -28,7 +31,7 @@ const Blog = () => {
         </div>
         <div className="my-10">
           <h2 className="font-bold">
-            2. Tell us the difference between nodejs and express js. ?
+            3. Tell us the difference between nodejs and express js. ?
           </h2>
           <p>
             <span className="text-red-600 font-bold">Ans :</span>  Node.js is a JavaScript runtime that provides the core functionality for building server-side applications in JavaScript, while Express.js is a web framework built on top of Node.js that provides additional tools and features specifically for building web applications and APIs.
@@ -36,14 +39,17 @@ const Blog = () => {
         </div>
         <div className="my-10">
           <h2 className="font-bold">
-            2. What is a custom hook, and why will you create a custom hook ?
+            4. What is a custom hook, and why will you create a custom hook ?
           </h2>
           <p>
             <span className="text-red-600 font-bold">Ans :</span> In React, a custom hook is a function that allows you to extract and reuse logic from your components. Custom hooks are created by combining built-in hooks or other custom hooks, and can be used to share functionality between components or to abstract away complex or repetitive logic.
           </p>
         </div>
       </div>
-      <button className="btn btn-primary">Download PDF</button>
+      <Pdf targetRef={ref} filename="code-example.pdf">
+        {({ toPdf }) => <button className="btn btn-primary" onClick={toPdf}>Generate Pdf</button>}
+      </Pdf>
+      {/* <button className="btn btn-primary">Download PDF</button> */}
     </div>
   );
 };
